02bb47f6e1b63909e68abf1660a7ad7d60f80cfc
t-vv <t.adams214@gmail.com>
2020-01-31 01:01:29 -0600

    set up REST API

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  private SERVER_URL = 'http://localhost:3000/products';
+
+  constructor(private httpClient: HttpClient) { }
+  public get() {
+    return this.httpClient.get(this.SERVER_URL);
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-home',
@@ -6,11 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
+  products = [];
+  constructor(private apiService: ApiService) { }
   ngOnInit() {
+    this.apiService.get().subscribe((data: any[]) => {
+      console.log(data);
+      this.products = data;
+    });
   }
-
 }
 
