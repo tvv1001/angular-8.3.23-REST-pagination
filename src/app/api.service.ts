7c41afb1f14462ce33b3a684d8954ca9587d3837
“[T V]” <t.adams214@gmail.com>
2020-03-01 23:22:11 -0600

    pagination updates

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpParams
+} from "@angular/common/http";
 
 import { throwError } from "rxjs";
-import { retry, catchError } from "rxjs/operators";
+import { retry, catchError, tap } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root"
 })
 export class ApiService {
   private SERVER_URL = "http://localhost:3000/products";
+
+  public first: string = "";
+  public prev: string = "";
+  public next: string = "";
+  public last: string = "";
 
   constructor(private httpClient: HttpClient) {}
 
@@ -28,10 +37,54 @@ export class ApiService {
     window.alert(errorMessage);
     return throwError(errorMessage);
   }
+  parseLinkHeader(header: string) {
+    if (header.length === 0) {
+      return;
+    }
+
+    const parts = header.split(",");
+    const links = {};
+    parts.forEach(p => {
+      const section = p.split(";");
+      const url = section[0].replace(/<(.*)>/, "$1").trim();
+      const name = section[1].replace(/rel="(.*)"/, "$1").trim();
+      links[name] = url;
+    });
+
+    this.first = links["first"];
+    this.last = links["last"];
+    this.prev = links["prev"];
+    this.next = links["next"];
+  }
 
   public sendGetRequest() {
+    // Add safe, URL encoded _page and _limit parameters
+
     return this.httpClient
-      .get(this.SERVER_URL)
-      .pipe(catchError(this.handleError));
+      .get(this.SERVER_URL, {
+        params: new HttpParams({ fromString: "_page=1&_limit=8" }),
+        observe: "response"
+      })
+      .pipe(
+        retry(3),
+        catchError(this.handleError),
+        tap(res => {
+          // console.log("header link");
+          // console.log(res.headers.get("Link"));
+          this.parseLinkHeader(res.headers.get("Link"));
+        })
+      );
+  }
+
+  public sendGetRequestToUrl(url: string) {
+    return this.httpClient.get(url, { observe: "response" }).pipe(
+      retry(3),
+      catchError(this.handleError),
+      tap(res => {
+        // console.log("list of all headers");
+        // console.log(res.headers.get("Link"));
+        this.parseLinkHeader(res.headers.get("Link"));
+      })
+    );
   }
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,18 +1,101 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { ApiService } from "../api.service";
+import { HttpResponse } from "@angular/common/http";
+import { takeUntil } from "rxjs/operators";
+import { Subject } from "rxjs";
 
 @Component({
   selector: "app-products",
   templateUrl: "./products.component.html",
   styleUrls: ["./products.component.scss"]
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements OnInit, OnDestroy {
   products = [];
+  destroy$: Subject<boolean> = new Subject<boolean>();
   constructor(private apiService: ApiService) {}
   ngOnInit() {
-    this.apiService.get().subscribe((data: any[]) => {
-      console.log(data);
-      this.products = data;
-    });
+    this.apiService
+      .sendGetRequest()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((res: HttpResponse<any>) => {
+        console.log("init response");
+        console.log(res);
+        this.products = res.body;
+      });
+  }
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    // Unsubscribe from the subject
+    this.destroy$.unsubscribe();
+  }
+
+  public firstPage() {
+    console.log("-------------------------");
+    console.log("first page");
+    console.log(this.apiService);
+    console.log(this.apiService.next);
+    console.log("-------------------------");
+
+    this.products = [];
+    this.apiService
+      .sendGetRequestToUrl(this.apiService.first)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((res: HttpResponse<any>) => {
+        console.log(res);
+        this.products = res.body;
+      });
+  }
+  public previousPage() {
+    console.log("-------------------------");
+    console.log("prev page");
+    console.log(this.apiService);
+    console.log(this.apiService.next);
+    console.log("-------------------------");
+
+    if (this.apiService.prev !== undefined && this.apiService.prev !== "") {
+      this.products = [];
+      this.apiService
+        .sendGetRequestToUrl(this.apiService.prev)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe((res: HttpResponse<any>) => {
+          console.log(res);
+          this.products = res.body;
+        });
+    }
+  }
+  public nextPage() {
+    console.log("-------------------------");
+    console.log("next page");
+    console.log(this.apiService);
+    console.log(this.apiService.next);
+    console.log("-------------------------");
+
+    if (this.apiService.next !== undefined && this.apiService.next !== "") {
+      this.products = [];
+      this.apiService
+        .sendGetRequestToUrl(this.apiService.next)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe((res: HttpResponse<any>) => {
+          // console.log("next response");
+          // console.log(res);
+          this.products = res.body;
+        });
+    }
+  }
+  public lastPage() {
+    console.log("-------------------------");
+    console.log("last page");
+    console.log(this.apiService);
+    console.log(this.apiService.next);
+    console.log("-------------------------");
+
+    this.products = [];
+    this.apiService
+      .sendGetRequestToUrl(this.apiService.last)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((res: HttpResponse<any>) => {
+        console.log(res);
+        this.products = res.body;
+      });
   }
 }
